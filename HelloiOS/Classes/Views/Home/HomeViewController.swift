d0de6098d3c4c9c0588112e5061b4a4e4e0a9df7
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-06-24 23:56:18 +0700

    Add sample screen

--- HelloiOS/Classes/Views/Home/HomeViewController.swift
+++ HelloiOS/Classes/Views/Home/HomeViewController.swift
@@ -4,11 +4,21 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        self.title = "Home"
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(
+            title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil
+        )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func goToDetail() {
+        let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
+        self.navigationController?.pushViewController(detailVC, animated: true)
     }
 }
